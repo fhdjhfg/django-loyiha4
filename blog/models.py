f583a58b812773d0dbee453a98cd0fb788f49910
muhammad <muhammadjonkarimberdiyev899@gmail.com>
2025-12-10 21:14:48 +0500

    taaaaaaa

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+class Yozuvchi (models.Model):
+    ism=models.CharField(max_length=200)
+    familiya=models.CharField(max_length=200,blank=True,null=True)
+    telefon_raqam=models.CharField(max_length=20,unique=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def str(self):
+        return self.ism 
+    
+
 
 # Create your models here.
